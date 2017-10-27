9ad27b6734eb73ce1215656b6153cd248d366968
philipfabianek <fabianekphilip@gmail.com>
2017-10-27 11:47:58 +0200

    Changed a lot.

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: __dirname + '/src/index.js',
     output: {
         path: __dirname + '/dist/assets',
-        filename: 'bundle.js',
+        filename: 'scripts.js',
         publicPath: 'assets'
     },
     devServer: {
@@ -46,6 +46,30 @@ module.exports = {
                     { loader: 'css-loader', options: { importLoaders: 1 } },
                     'sass-loader',
                     'postcss-loader'
+                ]
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loaders: [
+                    'file-loader?hash=sha512&digest=hex&name=[hash].[ext]', {
+                        loader: "image-webpack-loader",
+                        query: {
+                            mozjpeg: {
+                              progressive: true,
+                            },
+                            gifsicle: {
+                              interlaced: false,
+                            },
+                            optipng: {
+                              optimizationLevel: 4,
+                            },
+                            pngquant: {
+                              quality: '75-90',
+                              speed: 3,
+                            }
+                        }
+                    }
+                    // 'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
                 ]
             }
         ]
